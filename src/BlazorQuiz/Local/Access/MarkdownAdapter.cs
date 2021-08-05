4feb4226d698dd9e7577960fe61b7086b5d549ae
james <james.lee@devncore.org>
2021-08-05 17:40:22 +0900

    Update

---
+++ src/BlazorQuiz/Local/Access/MarkdownAdapter.cs
@@ -0,0 +1,34 @@
+﻿using Markdig;
+
+namespace BlazorQuiz.Local.Access
+{
+    public class MarkdownAdapter
+    {
+        public static MarkdownAdapter Instance;
+
+        static MarkdownAdapter()
+        {
+            Instance = new MarkdownAdapter();
+        }
+
+        private MarkdownAdapter()
+        { 
+            
+        }
+
+        private string Download(string url)
+        {
+            string contents;
+
+            using (var wc = new System.Net.WebClient())
+                contents = wc.DownloadString(url);
+            return contents;
+        }
+
+        internal string DownloadToHtml(string url)
+        {
+            string text = Download(url);
+            return Markdown.ToHtml(text);
+        }
+    }
+}

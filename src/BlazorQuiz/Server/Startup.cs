f58671f752fa1cf5366b7885c3e7ef13cc0429d3
james.lee <james.lee@devncore.org>
2021-08-05 22:12:16 +0900

    Update

--- src/BlazorQuiz/Server/Startup.cs
+++ src/BlazorQuiz/Server/Startup.cs
@@ -30,10 +30,10 @@ namespace BlazorQuiz.Server
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
-			app.UseDeveloperExceptionPage();
-			app.UseWebAssemblyDebugging();
 			if (env.IsDevelopment())
 			{
+				app.UseDeveloperExceptionPage();
+				app.UseWebAssemblyDebugging();
 			}
 			else
 			{
